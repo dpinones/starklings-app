c88abf96dee6e7a61374185a53abcd9a8a459160
nicon44 <nicolas@dulio.com.ar>
2023-11-01 21:33:42 +0100

    exercise variables 2

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import Editor from "@monaco-editor/react";
 import { Box, Button, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import "./App.css";
-import __wbg_init, {
-  compileCairoProgram,
-  runCairoProgram,
-} from "./pkg/module/wasm-cairo";
+import __wbg_init, { compileCairoProgram } from "./pkg/module/wasm-cairo";
 
-const cairoTestProgram = `use debug::PrintTrait;
-fn main() {
-    'Hello, StarknetAstro!'.print();
+const cairoTestProgram = `
+  use debug::PrintTrait;
+
+    fn main() {
+    let x;
+    if x == 10 {
+        ('x is ten!').print();
+    } else {
+        ('x is not ten!').print();
+    }
 }`;
 
 function App() {
   const [editorValue, setEditorValue] = useState(cairoTestProgram);
-  const [result, setResult] = useState("");
+  const [error, setError] = useState("");
+  const [passed, setPassed] = useState(false);
+  const [hint, setHint] = useState("");
 
   useEffect(() => {
     __wbg_init();
@@ -23,6 +29,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Typography sx={{ mb: 1 }} variant="h5">
+          Exercise: Variables 2
+        </Typography>
+        <Typography sx={{ mb: 2 }} variant="body1">
+          Fix the code
+        </Typography>
         <Editor
           onChange={(val) => setEditorValue(val)}
           theme="vs-dark"
@@ -35,17 +47,34 @@ function App() {
         <Box sx={{ display: "flex", gap: 2, my: 2 }}>
           <Button
             variant="contained"
+            disabled={hint !== ""}
+            onClick={() => {
+              setHint(`Hint: What happens if you annotate line 5 with a type annotation?
+              What if you give x a value?
+              What if you do both?
+              What type should x be, anyway? (remember what the basic type in Cairo is?)
+              What if x is the same type as 10? What if it's a different type? (e.g. a u8)`);
+            }}
+          >
+            Get hint
+          </Button>
+          <Button
+            variant="contained"
             onClick={() => {
               const result = compileCairoProgram(editorValue, false);
-              result.startsWith("failed to compile")
-                ? setResult(result)
-                : setResult("Compiled. See console");
+              if (result.startsWith("failed to compile")) {
+                setError(result)
+                setPassed(false)
+              } else {
+                setError('')
+                setPassed(true);
+              }
               console.log("compile result: ", result);
             }}
           >
             Compile
           </Button>
-          <Button
+          {/*           <Button
             variant="contained"
             onClick={() => {
               const result = runCairoProgram(
@@ -58,13 +87,26 @@ function App() {
             }}
           >
             Run
-          </Button>
+          </Button> */}
         </Box>
-        <Box>
-          {result !== "" && (
-            <Typography sx={{ textAlign: "left" }}>{result}</Typography>
-          )}
-        </Box>
+
+        {hint !== "" && (
+          <Box sx={{ m: 2 }}>
+            <Typography sx={{ textAlign: "left" }}>{hint}</Typography>
+          </Box>
+        )}
+
+        {error !== "" && (
+          <Box sx={{ m: 2 }}>
+            <Typography color='red' sx={{ textAlign: "left" }}>{error}</Typography>
+          </Box>
+        )}
+
+        {passed && (
+          <Box sx={{ m: 2 }}>
+            <Typography variant="h4" color='green'>Well done!</Typography>
+          </Box>
+        )}
       </header>
     </div>
   );
