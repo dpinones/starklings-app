5194aecca4d6e3bd0a748c1479c1642f5e92bda6
dpinones <pinonesdamian@gmail.com>
2023-11-23 20:00:16 -0300

    refactor logic endpoints groups

--- api/src/controllers/groups.controller.js
+++ api/src/controllers/groups.controller.js
@@ -1,20 +1,58 @@
 import { pool } from "../db.js";
 
 export const getAllGroups = async (req, res, next) => {
-  const result = await pool.query("SELECT * FROM groups");
-  return res.json(result.rows);
+  const result = [];
+  const resultGroups = await pool.query("SELECT * FROM groups");
+  const resultExercises = await pool.query("SELECT * FROM exercises");
+
+  resultGroups.rows.forEach(group => {
+
+    let groupRet = {};
+    groupRet.id = group.id;
+    groupRet.label = group.label; 
+    groupRet.exercises = [];
+
+    resultExercises.rows.forEach(exercise => {
+      if (group.id === exercise.groups) {
+        let exerciseRet = {};
+        exerciseRet.name = exercise.name;
+        exerciseRet.path = exercise.path;
+        exerciseRet.mode = exercise.modes;
+        groupRet.exercises.push(exerciseRet);
+      }
+    });
+    result.push(groupRet);
+  });
+
+  return res.json(result);
 };
 
 export const getGroup = async (req, res) => {
-  const result = await pool.query("SELECT * FROM groups WHERE id = $1", [
+  let result = {};
+  const resultGroup = await pool.query("SELECT * FROM groups WHERE id = $1", [
     req.params.id,
   ]);
+  
+  if (resultGroup.rowCount === 0) {
+    return res.status(404).json({
+      message: "No group exists with that id",
+    });
+  }
 
-  // if (result.rowCount === 0) {
-  //   return res.status(404).json({
-  //     message: "No existe un group con ese id",
-  //   });
-  // }
+  const resultExercises = await pool.query("SELECT * FROM exercises WHERE groups = $1", [
+    req.params.id
+  ]);
 
-  return res.json(result.rows[0]);
+  result.id = resultGroup.rows[0].id; 
+  result.label = resultGroup.rows[0].label;
+  result.exercises = [];
+  resultExercises.rows.forEach(exercise => {
+    let exerciseRet = {};
+    exerciseRet.name = exercise.name;
+    exerciseRet.path = exercise.path;
+    exerciseRet.mode = exercise.modes;
+    result.exercises.push(exerciseRet);
+  });
+
+  return res.json(result);
 };
