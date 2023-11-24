48e413712e230cb9cfce0a19252a32b099feaa7c
dpinones <pinonesdamian@gmail.com>
2023-11-24 09:01:44 -0300

    add hint endpoint

--- api/src/routes/exercises.routes.js
+++ api/src/routes/exercises.routes.js
@@ -2,11 +2,14 @@ import Router from "express-promise-router";
 import {
   getAllExercises,
   getExercise,
+  getHint,
 } from "../controllers/exercises.controller.js";
 
 const router = Router();
 
 router.get("/exercises", getAllExercises);
+
+router.get("/exercises/:id/hint", getHint);
 
 router.get("/exercises/:id", getExercise);
 
