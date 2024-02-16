48e621d61938bed7bbe4ee5950e5c06fefd5739c
dpinones <pinonesdamian@gmail.com>
2024-02-16 12:54:54 -0300

    add endpoint scarb test

--- api/src/routes/scarb.routes.js
+++ api/src/routes/scarb.routes.js
@@ -1,10 +1,13 @@
 import Router from "express-promise-router";
 import {
-    scarbBuild
+    scarbBuild,
+    scarbTest
 } from "../controllers/scarb.controller.js";
 
 const router = Router();
 
 router.post("/scarb/build/:user", scarbBuild);
+
+router.post("/scarb/test/:user", scarbTest);
 
 export default router;
