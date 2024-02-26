4049c0e58a8167cefd16657c0a0b2fe0947307e7
dpinones <pinonesdamian@gmail.com>
2024-02-26 00:56:42 -0300

    add table resolutions

--- database/init.sql
+++ database/init.sql
@@ -17,6 +17,14 @@ CREATE TABLE Exercises (
     exercise_order INTEGER
 );
 
+CREATE TABLE Resolutions (
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(255),
+    exercise_id VARCHAR(255) REFERENCES Exercises(id),
+    resolution_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (user_name, exercise_id)
+);
+
 INSERT INTO Groups (id, label) VALUES
     ('intro', 'Intro'),
     ('variables', 'Variables'),
