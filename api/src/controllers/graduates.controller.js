ff0abc21ec3c93df775dc7767b959f5ddae23651
Damian Piñones <pinonesdamian@gmail.com>
2024-05-14 10:52:41 -0300

    add checkGraduate

--- api/src/controllers/graduates.controller.js
+++ api/src/controllers/graduates.controller.js
@@ -6,3 +6,13 @@ export const getGraduates = async (req, res, next) => {
   );
   return res.json(result.rows);
 };
+
+export const checkGraduate = async (req, res, next) => {
+  let userGithub = req.params.github?.match(/^\d/) ? "gh" + req.params.github : req.params.github;
+  const result = await pool.query(
+    "SELECT COUNT(DISTINCT exercise_id) = 54 AS has_54_exercises FROM resolutions WHERE user_name = $1;", [userGithub]
+  );
+  return res.json({
+    "completed": result.rows[0].has_54_exercises
+  });
+};
