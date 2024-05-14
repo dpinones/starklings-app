a202e637c67defcecf9033b471ca9f2d6ba364ab
Damián Piñones <30808181+dpinones@users.noreply.github.com>
2024-05-14 15:53:47 +0200

    add checkGraduate (#169)

--- api/src/routes/graduates.routes.js
+++ api/src/routes/graduates.routes.js
@@ -1,10 +1,13 @@
 import Router from "express-promise-router";
 import {
     getGraduates,
+    checkGraduate
 } from "../controllers/graduates.controller.js";
 
 const router = Router();
 
 router.get("/graduates", getGraduates);
+
+router.get("/graduates/:github", checkGraduate);
 
 export default router;
