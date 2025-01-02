e65bbbad5f44a7a0cc26b77ff88054ad447ef339
nicon44 <nicolas@dulio.com.ar>
2025-01-02 19:37:04 -0300

    remove gh from graduates page

--- client/src/queries/useGetGraduates.tsx
+++ client/src/queries/useGetGraduates.tsx
@@ -8,9 +8,17 @@ export const useGetGraduates = () => {
     queryKey: ["graduates"],
     queryFn: async () => {
       const { data: graduates } = await axios.get(API_URL + "/graduates");
-      return [...graduates].filter(
-        (graduate: IGraduate) => graduate.user_name !== "cypress"
-      );
+      return [...graduates]
+        .filter((graduate: IGraduate) => graduate.user_name !== "cypress")
+        .map((graduate: IGraduate) => {
+          return {
+            ...graduate,
+            user_name: graduate.user_name.startsWith("gh")
+              ? graduate.user_name.slice(2)
+              : graduate.user_name,
+          };
+        })
+        .sort((a, b) => a.user_name.localeCompare(b.user_name));
     },
   });
 };
